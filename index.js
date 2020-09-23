9a49ffaf3a370713c54ccb2da634bb41e571a07b
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 22:33:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -46,12 +46,12 @@ function total() {
 }
 
 function removeFromCart(item) {
-    if( cart[itemName] === item ){
-      cart.splice()
+  for(let i=0; i<cart.length; i++){
+    if( cart[i].itemName === item ){
+      cart.splice(i,1,'')
       return cart
-    }else{
-      return `That item is not in your cart.`
     }
+  }
 }
 
 function placeOrder(cardNumber) {
