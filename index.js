a85c0e4806ce8d4b73833f19f6da5396f4c8909c
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 22:39:57 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,5 +56,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if( cardNumber === null ){
+    `Sorry, we don't have a credit card on file for you.`
+  }else{
+    let string = `Your total cost is $${total()}, which will be charged to card ${cardNumber}.`
+  }
 }
