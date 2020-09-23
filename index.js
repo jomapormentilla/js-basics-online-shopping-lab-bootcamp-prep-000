3b3c59ed27de1f304a94a46d4d4e32c4d0f0d122
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 22:40:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -57,9 +57,10 @@ function removeFromCart(item) {
 
 function placeOrder(cardNumber) {
   if( cardNumber === null ){
-    `Sorry, we don't have a credit card on file for you.`
+    return `Sorry, we don't have a credit card on file for you.`
   }else{
     cart = []
     let string = `Your total cost is $${total()}, which will be charged to card ${cardNumber}.`
+    return string
   }
 }
