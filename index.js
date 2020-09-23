ee43eb5b5679277ca7a1cf9009dcb57eb7381758
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 22:45:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  if( cardNumber === null ){
+  if( cardNumber === '' ){
     return `Sorry, we don't have a credit card on file for you.`
   }else{
     cart = []
