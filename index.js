b21b368d77706d09722663958fdece9ff02eb1a4
Joma Pormentilla <jomapormentilla@gmail.com>
2020-09-23 23:02:35 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -60,7 +60,7 @@ function placeOrder(cardNumber) {
     return `Sorry, we don't have a credit card on file for you.`
   }else{
     cart = []
-    let total = parseInt(total())
+    let total = total()
     let card = cardNumber
     let string = `Your total cost is $${total}, which will be charged to the card ${card}.`
     return string
